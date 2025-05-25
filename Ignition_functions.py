5ea74e2a249b323005a62fc7dd2d8f3483d7322c
Jordan Handy <166252434+jhandy03@users.noreply.github.com>
2025-05-24 22:45:17 -0400

    Updated Code

--- Ignition_functions.py
+++ Ignition_functions.py
@@ -1,1 +1,19 @@
+"""
+Author: Jordan Handy
+Last Updated: 05/24/2025
+Contact: jhandy03@vt.edu
+
+Written for MachWorks @ Virgnia Tech's Propulsions Subteam
+
+MIT Liscense or sum shit idk
+
+"""
+
+def aastar(self,M,gamma):
+    aas = (1/M)*(2/(gamma+1)*(1+((gamma-1)/2)*M^2))^((gamma+1)/(2*(gamma-1)))
+    return aas
+
+def stagtemp(self,M,gamma):
+    TT0 = (1+((gamma-1)/2)*M^2)^-1
+    return TT0
 
